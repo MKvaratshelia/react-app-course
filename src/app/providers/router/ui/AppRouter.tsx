f8e9844227fa4f574f3acae0b29d93781d6323c7
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2024-10-31 19:36:44 +0300

    add loader, notFoundPage

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,17 +1,22 @@
 import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
-export const AppRouter = () => (
-    <Suspense fallback={<div>...loading</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ path, element }) => (
-                <Route
-                    path={path}
-                    element={<div className="page-wrapper">{element}</div>}
-                    key={path}
-                />
-            ))}
-        </Routes>
-    </Suspense>
-);
+export const AppRouter = () => {
+    const { t } = useTranslation();
+    return (
+        <Suspense fallback={<PageLoader />}>
+            <Routes>
+                {Object.values(routeConfig).map(({ path, element }) => (
+                    <Route
+                        path={path}
+                        element={<div className="page-wrapper">{element}</div>}
+                        key={path}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    );
+};
